58aa613e7882b6984c59e9d7cd32041d18afee02
Joshua Britz <joshua.c.britz@gmail.com>
2026-05-14 14:59:01 +0200

    Handle Invalid commands
    Main loop logic in main funciton. Only recognizes exit as a valid
    command

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,28 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+
+    main_loop();
+}
+
+fn main_loop() {
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let mut cmd = String::new();
+
+        io::stdin()
+            .read_line(&mut cmd)
+            .expect("Failed to readline.");
+
+        let cmd = cmd.trim();
+
+        match cmd {
+            "exit" => return,
+            _ => {
+                println!("{}: command not found", cmd);
+            }
+        }
+    }
 }
