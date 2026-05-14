a8083b6911dc2643f3b0b615b2c0de6ca5ed77ff
Joshua Britz <joshua.c.britz@gmail.com>
2026-05-14 17:00:00 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -88,13 +88,13 @@ fn find_executables(path: &str) -> HashMap<String, String> {
         .iter()
         .flat_map(|dir| std::fs::read_dir(dir).unwrap())
         .filter_map(|entry| entry.ok())
-        .filter(|entry| entry.metadata().unwrap().mode() == 0o001)
+        .filter(|entry| entry.metadata().unwrap().mode() & 0o001 != 0)
         .collect::<Vec<_>>();
 
     // split into executables and subdirectories
     let execs = subdirs_and_execs
         .iter()
-        .filter(|entry| entry.metadata().unwrap().mode() == 0o001);
+        .filter(|entry| entry.metadata().unwrap().mode() & 0o001 != 0);
 
     for exec in execs {
         let name = exec.file_name().to_string_lossy().into_owned();
